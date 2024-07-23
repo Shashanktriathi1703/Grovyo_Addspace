db2ac25de761b39df1cbbe081990a109d9528077
Shashank Tripathi <105815482+Shashanktriathi1703@users.noreply.github.com>
2024-07-23 23:11:18 +0530

    File Uploading Successfully

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <h1 className="font-bold text-[50px] text-white whitespace-pre-wrap">Shashank</h1>
+     
+    </main>
+  );
+}
